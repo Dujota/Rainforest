3708a96cf9dd7cbd3d53d56957dfaadadf9541a7
Tony <ANCLIST@GMAIL.COM>
2017-10-18 19:10:43 -0400

    Make prices look pretty

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ApplicationRecord
 
   validates :name, :description, :price, presence: true
   validates :price, numericality: { only_integer: true }
+
+
+  def price_in_dollars
+  price_in_dollars = price.to_f / 100
+  sprintf("$%.2f", price_in_dollars)
+  end
+
+
 end
