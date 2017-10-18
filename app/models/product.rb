2b83e061b1f27fdfc21ed7d085241e058cac7b39
Tony <ANCLIST@GMAIL.COM>
2017-10-18 19:42:39 -0400

    Created the Review Model including the product foreign key, establish association between product and reviews and it all worked fine

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,8 @@ class Product < ApplicationRecord
   validates :name, :description, :price, presence: true
   validates :price, numericality: { only_integer: true }
 
-
+  has_many :reviews
+  
   def price_in_dollars
   price_in_dollars = price.to_f / 100
   sprintf("$%.2f", price_in_dollars)
