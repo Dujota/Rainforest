36b753995beae74302f704171bb97391096b9118
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-18 14:00:15 -0400

    empty controller functions created to match the CRUD in rails routes

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+
+  def index
+  end
+
+  def create
+    #code
+  end
+
+  def new
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+end
