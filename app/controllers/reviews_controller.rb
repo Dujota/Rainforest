050b31db6827432edfee66e216b4bbd304f1dd9a
Tony <ANCLIST@GMAIL.COM>
2017-10-19 15:58:28 -0400

    Save the new review and redirect to the product show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,17 @@
 class ReviewsController < ApplicationController
   def create
-    
+    @review = Review.new
+    @review.comment = params[:review][:comment]
+    @review.product_id = params[:product_id]
+    @product = Product.find(params[:product_id])
+    @reviews = @product.reviews
+
+    if @review.save
+        flash[:notice] = "You have successfully added a new review."
+      redirect_to product_path(@product)
+    else
+      render '/products/show'
+    end
   end
 
   def edit
